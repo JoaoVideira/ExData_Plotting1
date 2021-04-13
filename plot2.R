9e79dbffc3fb687f1c760a6eea270874cd12252c
Joao Videira <joaoamvideira@gmail.com>
2021-04-13 18:39:33 -0300

    Segundo Gráfico

---
+++ plot2.R
@@ -0,0 +1,21 @@
+#Lendo, nomeando e e selecioando data sobre poder de consumo
+power <- read.table("household_power_consumption.txt",skip=1,sep=";")
+names(power) <- c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3")
+subpower <- subset(power,power$Date=="1/2/2007" | power$Date =="2/2/2007")
+
+# Transformando vars Date e Time
+subpower$Date <- as.Date(subpower$Date, format="%d/%m/%Y")
+subpower$Time <- strptime(subpower$Time, format="%H:%M:%S")
+subpower[1:1440,"Time"] <- format(subpower[1:1440,"Time"],"2007-02-01 %H:%M:%S")
+subpower[1441:2880,"Time"] <- format(subpower[1441:2880,"Time"],"2007-02-02 %H:%M:%S")
+
+
+# usando funcao plot para gerar grafico
+plot(subpower$Time,as.numeric(as.character(subpower$Global_active_power)),type="l",xlab="",ylab="Global Active Power (kilowatts)") 
+
+# colocando titulo no grafico
+title(main="Global Active Power Vs Time")
+
+#copiando para png
+dev.copy(png, filename="plot2.png")
+dev.off()
